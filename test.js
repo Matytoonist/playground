f83aa0d34b9e21ae702d308206e1d60815222fb9
Matytoonist <matytoonist@gmail.com>
2024-10-14 09:29:46 -0300

    removed AI trash

--- test.js
+++ test.js
@@ -1,30 +1,3 @@
-// Make the DIV element draggable:
-document.querySelectorAll('.dragable-window').forEach(window => {
-    let offsetX, offsetY;
-
-    window.addEventListener('mousedown', (e) => {
-        offsetX = e.clientX - window.getBoundingClientRect().left;
-        offsetY = e.clientY - window.getBoundingClientRect().top;
-
-        window.style.cursor = 'grabbing';
-        
-        const mouseMoveHandler = (e) => {
-            window.style.left = `${e.clientX - offsetX}px`;
-            window.style.top = `${e.clientY - offsetY}px`;
-        };
-
-        const mouseUpHandler = () => {
-            window.style.cursor = 'move';
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        };
-
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    });
-});
-
-
 function closeWindow(id){
   document.getElementById(id).style.display = 'none';
   }
