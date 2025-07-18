f51a97d94a106856ddf383cb6e6ecb919de88a90
Matytoonist <matytoonist@gmail.com>
2025-07-18 18:09:51 -0300

    stupid-simple little guy that greets you

---
+++ java warmups/greet.java
@@ -0,0 +1,7 @@
+public class greet{
+    public static void main(String[] args) {
+        System.out.println("\\o -(Hola " + args[0] + "!)");
+        System.out.println(" |\\");
+        System.out.println("/ \\");
+    }
+}
